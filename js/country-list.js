8a1cb075c3f1480274fb40d90b2f2b46b5878c8f
Olufisayo Israel Tomide <joshuatemitayo97@gmail.com>
2023-12-22 14:03:29 +0100

    fix

---
+++ js/country-list.js
@@ -0,0 +1,11 @@
+const country_code = {
+    "USD": "United States (US)",
+    "EUR": "Eurozone (EU)",
+    "JPY": "Japan (JP)",
+    "GBP": "United Kingdom (UK)",
+    "AUD": "Australia (AU)",
+    "CAD": "Canada (CA)",
+    "INR": "India (IN)",
+    "CNY": "China (CN)",
+    "BRL": "Brazil (BR)"
+}
